05d4526a3e8c04145e3d6f834826e1b54381704b
dest411 <vadimstrizhak652@gmail.com>
2025-07-13 01:06:54 +0300

    ptichka completed

--- reservation.js
+++ reservation.js
@@ -3,3 +3,33 @@ const arrow2 = document.querySelector('.arrowbacktwo');
   arrow2.addEventListener('click', () => {
     document.getElementById('toptwo').scrollIntoView({ behavior: 'smooth' });
   });
+  
+function toggleselect(selectcitynone) {
+    const value = window.getComputedStyle(selectcitynone).display;
+
+    const typesOfHouses = document.querySelector('.types-of-houses');
+    const ptichka = document.querySelector('.ptchk');
+
+    if (value === 'none') {
+        selectcitynone.style.display = 'flex';
+
+        if (typesOfHouses) {
+            typesOfHouses.style.marginTop = '200px';
+        }
+        if (ptichka) {
+            ptichka.style.transform = 'rotate(180deg)';
+            ptichka.style.transition = 'transform 0.3s ease';
+        }
+    }
+    else if (value === 'flex') {
+        selectcitynone.style.display = 'none';
+
+        if (typesOfHouses) {
+            typesOfHouses.style.marginTop = '';
+        }
+        if (ptichka) {
+            ptichka.style.transform = 'rotate(0deg)';
+            ptichka.style.transition = 'transform 0.3s ease';
+        }
+    }
+}
